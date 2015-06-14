e92be85b58786c6907019d11914673ef57d82691
Bai Jie <baijie1991@gmail.com>
2015-06-14 10:58:23 +0800

    add CheckFileInformation Use Case

---
+++ business/src/main/java/gq/baijie/catalog/usecase/CheckFileInformation.java
@@ -0,0 +1,112 @@
+package gq.baijie.catalog.usecase;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import gq.baijie.catalog.entity.DirectoryFile;
+import gq.baijie.catalog.entity.File;
+import gq.baijie.catalog.entity.Hash;
+import gq.baijie.catalog.entity.RegularFile;
+
+public class CheckFileInformation implements UseCase {
+
+    @Nonnull
+    private final DirectoryFile rootDirectoryFile;
+
+    @Nonnull
+    private final FileCheckerListener fileCheckerListener;
+
+    public CheckFileInformation(
+            @Nonnull DirectoryFile rootDirectoryFile,
+            @Nonnull FileCheckerListener fileCheckerListener
+    ) {
+        this.rootDirectoryFile = rootDirectoryFile;
+        this.fileCheckerListener = fileCheckerListener;
+    }
+
+    @Override
+    public void execute() {
+        checkFiles();
+    }
+
+    private void checkFiles() {
+        final EnumMap<Hash.Algorithm, MessageDigest> messageDigestCache =
+                new EnumMap<>(Hash.Algorithm.class);
+        checkFiles(rootDirectoryFile, messageDigestCache);
+    }
+
+    @Nonnull
+    private CheckResult checkFiles(
+            @Nonnull DirectoryFile directory,
+            @Nonnull Map<Hash.Algorithm, MessageDigest> messageDigestCache
+    ) {
+        CheckResult result = CheckResult.CONTINUE;
+        for (final File file : directory.getContent()) {
+            if (file instanceof DirectoryFile) {
+                result = checkFiles((DirectoryFile) file, messageDigestCache);
+            } else if (file instanceof RegularFile) {
+                result = checkFile((RegularFile) file, messageDigestCache);
+            } else {
+                throw new UnsupportedOperationException("unknown instance of File:" + file);
+            }
+            if (result == CheckResult.TERMINATE) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Nonnull
+    private CheckResult checkFile(
+            @Nonnull final RegularFile file,
+            @Nonnull Map<Hash.Algorithm, MessageDigest> messageDigestCache
+    ) {
+        List<Hash> fileHashs = file.getHashs();
+        final Hash[] hashResultContainer = new Hash[fileHashs.size()];
+        final Hash.Algorithm[] algorithms = new Hash.Algorithm[fileHashs.size()];
+        int count = 0;
+        for (Hash hash : fileHashs) {
+            algorithms[count++] = hash.getAlgorithm();
+        }
+        try {
+            new HashFile(file.getPath(), algorithms, messageDigestCache, hashResultContainer)
+                    .execute();
+        } catch (RuntimeException e) {
+            if (e.getCause() instanceof IOException) {
+                return fileCheckerListener.onCheckFileFailed(file, (IOException) e.getCause());
+            } else {
+                throw e;
+            }
+        }
+        return fileCheckerListener.onFileChecked(file, hashResultContainer);
+    }
+
+
+    public static interface FileCheckerListener {
+
+        @Nonnull
+        public CheckResult onCheckFileFailed(RegularFile file, IOException exception);
+
+        /**
+         * called after the file is checked
+         *
+         * @param file      the file on check
+         * @param realHashs current hash value of the file on the file system.
+         *                  Its order is same to the order of {@code file.getHashs()}
+         */
+        @Nonnull
+        public CheckResult onFileChecked(RegularFile file, Hash[] realHashs);
+
+    }
+
+    public static enum CheckResult {
+        CONTINUE,
+        TERMINATE
+    }
+
+}
