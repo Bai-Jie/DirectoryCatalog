06be1bc66acd4c2bb32e460afb1b2b0beab7cd79
Bai Jie <baijie1991@gmail.com>
2015-07-21 11:50:27 +0800

    improve the HEX util class

--- domain/src/main/java/gq/baijie/catalog/util/HEX.java
+++ domain/src/main/java/gq/baijie/catalog/util/HEX.java
@@ -1,26 +1,31 @@
 // copy http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java#9855338
 package gq.baijie.catalog.util;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+
+import javax.annotation.Nonnull;
 
 public class HEX {
 
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
-    private static final Map<Character, Integer> HEX_TO_INTEGER = new HashMap<>();
+    private static final Map<Character, Integer> HEX_TO_INTEGER;
 
     static {
+        final Map<Character, Integer> tempHexToInteger = new HashMap<>();
         for (int i = 0; i < HEX_ARRAY.length; i++) {
-            HEX_TO_INTEGER.put(HEX_ARRAY[i], i);
+            tempHexToInteger.put(HEX_ARRAY[i], i);
         }
+        HEX_TO_INTEGER = Collections.unmodifiableMap(tempHexToInteger);
     }
 
     private HEX() {
     }
 
-    public static String bytesToHex(byte[] bytes) {
+    public static String bytesToHex(@Nonnull byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
@@ -30,7 +35,7 @@ public class HEX {
         return new String(hexChars);
     }
 
-    public static byte[] hexToBytes(String hex) {
+    public static byte[] hexToBytes(@Nonnull String hex) {
         if (hex.length() % 2 != 0) {
             throw new IllegalArgumentException(
                     "illegal hex: length of hex string isn't even number");
